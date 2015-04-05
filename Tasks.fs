1f09c0e2bcc400f551b754b5d435d0c37aa77ecd
Sharganov Artem <sharganov.artem@yandex.ru>
2015-04-06 00:21:57 +0400

    New version
    1.Исправлена ошибка со степенью
    Ожидаются тесты для ToPostfix

--- Tasks.fs
+++ Tasks.fs
@@ -1,6 +1,30 @@
 open System.IO
 open NUnit.Framework
 
+type Token = 
+  |Num of float
+  |LB 
+  |RB 
+  |Oper of char
+  |Con of string
+
+
+let pr (s:Token) =
+  match s with 
+  |Oper('+') -> 2
+  |Oper('-') -> 2
+  |Oper('/') -> 3
+  |Oper('*') -> 3
+  |Oper('^') -> 4 
+  |Oper('%') -> 3 
+  |_-> - 1 
+
+      
+let rassoc (c:char) =
+ match c with 
+ |'^' -> true
+ |_ -> false
+
 let ReadFrom (filename : string) = 
   use stream = new StreamReader(filename)
   let str = stream.ReadToEnd ()
@@ -10,118 +34,100 @@ let WriteTo (filename : string) (str:string) =
   use stream = new StreamWriter (filename)
   stream.Write (str)
   
-let pr x =
-  match x with 
-  |'(' -> 1
-  |'+' -> 2
-  |'-' -> 2
-  |'/' -> 3
-  |'*' -> 3
-  |'^' -> 4 
-  |'%' -> 3 
-  |x -> match System.Char.IsLetter x with
-        |res when res = true -> 0
-        |_ -> - 1
 
-
-type Stack<'A> = 
-  interface
-    abstract deletetop: unit -> bool
-    abstract push : 'A -> bool 
-    abstract pop  : unit -> Option<'A>
-    abstract isEmpty : unit -> bool
-    abstract size : unit -> int
-    abstract top  : unit -> Option<'A>
-    abstract print : unit -> unit  
-  end
-
-type ListStack<'A> () = 
+type Stack<'A> () = 
   class
-    let mutable list = []
-    interface Stack<'A> with 
-      member s.print () =
-        printfn "%A" list
-      member s.deletetop () = 
-        if list.Length = 0 then false
-        else list <- list.Tail; true
-      member s.size () = list.Length
-      member s.push a = list <- a ::list; true
-      member s.isEmpty() = list.IsEmpty 
-      member s.top () = 
-        if list.IsEmpty then None else Some list.Head 
-      member s.pop () = 
-        if list.IsEmpty
-        then None
-        else 
-          let head = list.Head
-          list <- list.Tail
-          Some head
+    let mutable list = [] 
+    member s.print () =
+      printfn "%A" list
+    member s.deletetop () = 
+      if list.Length = 0 then false
+      else list <- list.Tail; true
+    member s.size () = list.Length
+    member s.push (a:'A) = list <- a ::list; true
+    member s.isEmpty() = list.IsEmpty 
+    member s.top () = 
+      if list.IsEmpty then None else Some list.Head 
+    member s.pop () = 
+      if list.IsEmpty
+      then None
+      else 
+        let head = list.Head
+        list <- list.Tail
+        Some head
   end
 
 
-let toPostfix (s : string) =
-  use ostream = new StreamWriter "otest.txt"
-  let input = ReadFrom s
-  let stack =  new ListStack<char>():>Stack<char>
-  let mutable i = 0
-  let mutable lenght = input.Length - 1
-  let mutable buffer = ""
-  let mutable flag = false
-  while lenght >= 0 do
-     match input.[i] with
-     |')' -> while(stack.top().Value <> '(') do 
-               ostream.WriteLine (stack.pop().Value)
-             ignore(stack.deletetop())
-             i <- i + 1
-             lenght <- lenght - 1
-            
-     | _ ->  
-               buffer <- ""
-               if (System.Char.IsDigit input.[i])||pr input.[i] = 0 then 
-                 if flag = true then 
-                   buffer <- buffer + "-"
-                   flag <- false
-                 buffer <- buffer + input.[i].ToString()
-                 i <- i + 1
-                 lenght <- lenght - 1
-                 while (lenght >= 0)&&((System.Char.IsDigit input.[i])||pr input.[i] = 0) do 
-                   buffer <- buffer + input.[i].ToString()
-                   i <- i + 1
-                   lenght <- lenght - 1
-                 ostream.WriteLine buffer
-               elif (input.[i] = ' ') then 
-                 i <- i + 1
-                 lenght <- lenght - 1
-               elif input.[i] = '(' then 
-                 if (input.[i+1] = '-') then
-                   flag <- true
-                   ignore(stack.push input.[i])
-                   i <- i + 2
-                   lenght <- lenght - 2
-                 else 
-                   ignore(stack.push input.[i])
-                   i <- i + 1
-                   lenght <- lenght - 1
-               elif stack.isEmpty() || pr input.[i] > pr (stack.top().Value) then 
-                 ignore(stack.push input.[i])
-                 i <- i + 1
-                 lenght <- lenght - 1
-               elif pr input.[i] <= pr (stack.top().Value) then
-                 while  (stack.isEmpty()<>true)&&(pr input.[i] <= pr (stack.top().Value)) do
-                   ostream.WriteLine (stack.pop().Value)
-                 ignore(stack.push input.[i])
-                 i <- i + 1
-                 lenght <- lenght - 1
-                      
-  while (stack.isEmpty() <> true) do 
-    ostream.WriteLine (stack.pop().Value)
-  
 
+let parse (input : string) =
+  let length = input.Length - 1
+  let mutable i = 0
+  let mutable buffer = ""
+  let mutable tlist =  []
+  let mutable flag = false
+  while i <= length do
+    match input.[i] with
+    |'_' -> tlist<-tlist
+    |'+' -> tlist <- Oper('+')::tlist
+    |'-' ->if(input.[i-1] = '(') 
+             then flag <- true
+           else  
+             tlist <- Oper('-')::tlist
+    |'/' -> tlist <- Oper('/')::tlist
+    |'*' -> tlist <- Oper('*')::tlist
+    |'^' -> tlist <- Oper('^')::tlist
+    |'%' -> tlist <- Oper('%')::tlist
+    |')' -> tlist <- RB::tlist
+    |'(' -> tlist <- LB::tlist
+    |_ -> buffer <- ""
+          while (i<=length)&&((System.Char.IsDigit input.[i])||System.Char.IsLetter input.[i]) do 
+            buffer <- buffer + input.[i].ToString()
+            i<-i+1
+          if flag then 
+            buffer <- "-" + buffer
+            flag <- false
+          if System.Char.IsDigit (buffer.[0])||buffer.[0] = '-' then 
+            tlist <-Num(System.Convert.ToDouble buffer)::tlist
+          else 
+            tlist<- Con(buffer)::tlist
+          i<-i-1
+    i<-i+1     
+  tlist <- List.fold (fun acc i -> i::acc) [] tlist
+  tlist
+
+let getval (s:Token) = 
+  match s with 
+  |Oper(x) -> x
+  |_-> 'N'
+let ToPostfix (fn:string) =
+  use ostream = new StreamWriter "otest.txt"
+  let str = ReadFrom fn
+  let mutable tlist = parse str
+  let mutable stack = new Stack<Token>()
+  while (not tlist.IsEmpty) do 
+    match tlist.Head with
+    |Num(x) -> ostream.WriteLine x
+    |Con(s) -> ostream.WriteLine s
+    |LB -> ignore(stack.push LB)
+    |RB -> while stack.top().Value <> LB do
+            ostream.WriteLine (getval(stack.pop().Value))
+           ignore(stack.deletetop())
+    |Oper(s) -> 
+                if rassoc s then 
+                  while (not(stack.isEmpty()))&&(pr (Oper(s)) < pr (stack.top().Value)) do 
+                    ostream.WriteLine (getval(stack.pop().Value))
+                else 
+                  while (not(stack.isEmpty()))&& (pr (Oper(s)) <= pr (stack.top().Value)) do
+                    ostream.WriteLine (getval(stack.pop().Value))
+                ignore(stack.push (Oper(s)))
+    tlist <- tlist.Tail
+  while (not (stack.isEmpty())) do 
+    ostream.WriteLine (getval(stack.pop().Value))
 
 let stackmachine (fn : string) = 
   use instream = new StreamReader (fn)
   use outstream = new StreamWriter "result.txt"
-  let stack = new ListStack<float>():>Stack<float>
+  let stack = new Stack<float>()
   while not instream.EndOfStream do 
     let str = instream.ReadLine()
     
@@ -148,20 +154,28 @@ let stackmachine (fn : string) =
           ignore(stack.push (System.Convert.ToDouble str))
   outstream.Write (stack.top().Value)
 
+[<TestCase ("3^1^2", Result = 3.0)>]
 [<TestCase ("2+2*2", Result = 6.0)>]
 [<TestCase ("3-(-2)", Result = 5.0)>]
 [<TestCase ("(17/34)^5*32", Result = 1.0)>]
 [<TestCase ("12/(2*3)", Result = 2)>]
-[<TestCase ("3+7*(1-5)^2^3/1024", Result = 31.0)>]
 [<TestCase ("19+(2%4)^2", Result = 23.0)>]
 [<TestCase ("(19+18*(2+6)/3^2)%4", Result = 3.0)>]
-[<TestCase ("(3+4)^2 - (5 - 7)^3", Result = 57.0)>]
+[<TestCase ("(3+4)^2-(5-7)^3", Result = 57.0)>]
 let test value = 
   WriteTo "test.txt" value 
-  toPostfix "test.txt"
+  ToPostfix "test.txt"
   stackmachine "otest.txt"
   let str = ReadFrom "result.txt"
   System.Convert.ToDouble str
+
+[<TestCase ("4/2", Result = "4\r\n2\r\n/\r\n")>]
+let test01 value = 
+  WriteTo "test.txt" value 
+  ToPostfix "test.txt"
+  let str = ReadFrom "otest.txt"
+  str
+
 [<EntryPoint>]
 let main argv = 
   0 
