9a916d5e029c486a73a582a0ad76d3484814dfc8
sharganov <sharganov.artem@yandex.ru>
2015-04-30 21:22:19 +0400

    Add MergeSort

---
+++ hw09/maxInArray/src/app/fun.fs
@@ -0,0 +1,77 @@
+﻿module Program
+
+open System.Threading
+
+let maxInRange (arr : int []) l r : int =
+  let mutable res = 0
+  for i in l .. r do
+    if arr.[i] > res then 
+      res <- arr.[i]
+  res
+
+let maxInArray threadNumber (array : int []) =
+  let arraySize = array.Length
+  if threadNumber <= 0 then 
+     printfn "Not enough threads"
+     None
+  else
+      let res = ref 0
+      let step = arraySize / threadNumber
+      let apend = arraySize % threadNumber
+      let threadArray = Array.init threadNumber (fun i ->
+          new Thread(ThreadStart(fun _ ->
+              let threadRes = maxInRange array (i * step) ((i+1) * step + apend - 1 )
+              Monitor.Enter(res)
+              if threadRes > res.Value then 
+                res := threadRes
+              Monitor.Exit(res)
+                   ))
+        )
+      for t in threadArray do
+        t.Start()
+      for t in threadArray do
+        t.Join()
+      Some res.Value
+
+
+let split (arr : 'a array) =
+    let n = arr.Length
+    arr.[0..n/2-1], arr.[n/2..n-1]
+ 
+let rec merge (arr1 : 'a array) (arr2 : 'a array) =
+    let n = arr1.Length + arr2.Length
+    let res = Array.zeroCreate<'a> n
+    let mutable j = 0
+    let mutable i = 0
+    for k = 0 to n-1 do
+      if i >= arr1.Length then res.[k] <- arr2.[j]; j <- j + 1
+      elif j >= arr2.Length then res.[k] <- arr1.[i]; i <- i + 1
+      elif arr1.[i] < arr2.[j] then res.[k] <- arr1.[i]; i <- i + 1
+      else res.[k] <- arr2.[j]; j <- j + 1
+ 
+    res
+ 
+let rec pmergesort (threadMaxNumber : int) (arr : int []) =
+    match arr with 
+    | [||] -> [||]
+    | [|a|] -> [|a|]
+    | arr ->
+       let (l,r) = split arr
+       if threadMaxNumber >1 then
+         let r1 = ref [||]
+         let r2 = ref [||]   
+        
+         let mutable  res = Array.zeroCreate (l.Length + r.Length)
+         let rThread = new Thread(ThreadStart(fun _ ->
+                r1 := pmergesort (threadMaxNumber / 2) l
+               ))
+         rThread.Start()
+         r2 := pmergesort (threadMaxNumber / 2) r
+         rThread.Join()
+         merge r1.Value r2.Value
+       else 
+         merge  (pmergesort (threadMaxNumber / 2) l) (pmergesort (threadMaxNumber/2) r)
+   
+[<EntryPoint>]
+let main argv = 
+    0 
--- hw09/maxInArray/src/test/test.fs
+++ hw09/maxInArray/src/test/test.fs
@@ -9,7 +9,7 @@ open Program
 [<TestCase (5, [|9;5;2;3;4;5;6;7;8|], Result = 9)>]
 
 let test threadNum arr =
-  maxInArray threadNum arr
+  (maxInArray threadNum arr).Value
 
 (*
 Запуск с различными входными данными:
@@ -18,5 +18,34 @@ let test threadNum arr =
 (maxInArray 8 [|for i in 0..10000000 -> i|]) ==> 2.603
 (maxInArray 1 [|for i in 0..100000000 -> i|]) ==> 19.567
 (maxInArray 4 [|for i in 0..100000000 -> i|]) ==> 18.261
+*)
 
+let check (arr: 'a []) = 
+  if arr = [||] then true
+  else 
+    let length = arr.Length - 1
+    let mutable flag = true
+    let mutable i = 0
+    while i < (length - 1)  && flag do
+      if arr.[i] <= arr.[i+1] then 
+        i<- i+1
+        flag <- true
+      else flag <- false
+    flag
+     
+[<TestCase (1, [|9;8;7;6;5;4;3;2;1;0|], Result = true)>]
+[<TestCase (4, [|0;1;2;3;4;5|], Result = true)>]
+[<TestCase (2, [|0;1;0;1;0|], Result = true)>]
+[<TestCase (3, ([||] :int []), Result = true)>]
+let test2 n arr = 
+  pmergesort n arr |> check
+  
+(*Запуски на больших данных:
+pmergesort 1 [|for i in 0..1000000 -> 1000000 - i|] ==> 2.843
+pmergesort 2 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.666
+pmergesort 4 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.342
+pmergesort 8 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.347
+pmergesort 1 [|for i in 0..10000000 -> 10000000 - i|] ==> 33.476
+pmergesort 2 [|for i in 0..10000000 -> 10000000 - i|] ==> 23.369
+pmergesort 4 [|for i in 0..10000000 -> 10000000 - i|] ==> 15.462
 *)
