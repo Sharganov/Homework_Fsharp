a3f82dfc621567887fa687f1aca5f7a6f196c190
Sharganov Artem <sharganov.artem@yandex.ru>
2015-05-04 02:31:43 +0400

    Update test.fs

--- hw09/Arrayfun/src/test/test.fs
+++ hw09/Arrayfun/src/test/test.fs
@@ -1,13 +1,26 @@
-﻿module Test
+module Test
 open NUnit.Framework
 open Program
+
+[<Test>]
+let ``Max_Test 1 thread`` () =
+  let n = 1000000 
+  let rnd = new System.Random(0)
+  let g = Array.init n (fun i -> rnd.Next(0, n))
+  Assert.AreEqual(999999, (maxInArray 1 g).Value)
+
+[<Test>]
+let ``Max_Test 2 threads`` () =
+  let n = 1000000 
+  let rnd = new System.Random(0)
+  let g = Array.init n (fun i -> rnd.Next(0, n))
+  Assert.AreEqual(999999, (maxInArray 2 g).Value)
 
 [<TestCase (4, [|3;4;6;24;4;2;23;34;5;6;7;6;54|], Result = 54)>]
 [<TestCase (1, [|3;4;6;24;4;2;23;34;5;6;7;6;54|], Result = 54)>]
 [<TestCase (2, [|0;0;0;0;0;0;0|], Result = 0)>]
 [<TestCase (3, [|9;5;2;3;4;5;6;7;8|], Result = 9)>]
 [<TestCase (5, [|9;5;2;3;4;5;6;7;8|], Result = 9)>]
-
 let test threadNum arr =
   (maxInArray threadNum arr).Value
 
@@ -32,15 +45,33 @@ let check (arr: 'a []) =
         flag <- true
       else flag <- false
     flag
-     
+    
 [<TestCase (1, [|9;8;7;6;5;4;3;2;1;0|], Result = true)>]
 [<TestCase (4, [|0;1;2;3;4;5|], Result = true)>]
 [<TestCase (2, [|0;1;0;1;0|], Result = true)>]
 [<TestCase (3, ([||] :int []), Result = true)>]
 let test2 n arr = 
   pmergesort n arr |> check
+
+[<Test>]
+let ``mergesort 1 thread`` () =
+  let n = 1000000 
+  let rnd = new System.Random(0)
+  let g = Array.init n (fun i -> rnd.Next(0, n))
+  Assert.AreEqual(true, pmergesort 1 g |> check)
+
+
+[<Test>]
+let ``mergesort 3 thread`` () =
+  let n = 1000000 
+  let rnd = new System.Random(0)
+  let g = Array.init n (fun i -> rnd.Next(0, n))
+  Assert.AreEqual(true, pmergesort 1 g |> check)
+
   
 (*Запуски на больших данных:
+
+До переработки:
 pmergesort 1 [|for i in 0..1000000 -> 1000000 - i|] ==> 2.843
 pmergesort 2 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.666
 pmergesort 4 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.342
@@ -48,4 +79,13 @@ pmergesort 8 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.347
 pmergesort 1 [|for i in 0..10000000 -> 10000000 - i|] ==> 33.476
 pmergesort 2 [|for i in 0..10000000 -> 10000000 - i|] ==> 23.369
 pmergesort 4 [|for i in 0..10000000 -> 10000000 - i|] ==> 15.462
+
+После:
+pmergesort 1 [|for i in 0..1000000 -> 1000000 - i|] ==> 0.358
+pmergesort 2 [|for i in 0..1000000 -> 1000000 - i|] ==> 0.249
+pmergesort 4 [|for i in 0..1000000 -> 1000000 - i|] ==> 0.169
+pmergesort 8 [|for i in 0..1000000 -> 1000000 - i|] ==> 1.347
+pmergesort 1 [|for i in 0..10000000 -> 10000000 - i|] ==> 3.592
+pmergesort 2 [|for i in 0..10000000 -> 10000000 - i|] ==> 2.458
+pmergesort 4 [|for i in 0..10000000 -> 10000000 - i|] ==> 2.201
 *)
